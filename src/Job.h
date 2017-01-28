adf6dfd963f8e2e918e4f9a6670444772f29fd9b
LiavElisha <liavelisha@gmail.com>
2017-01-28 19:02:08 +0200

    1 using thread pool design in order to compute the shortest way 2 if there is not a path between 2 points the trip time is -1

---
+++ src/Job.h
@@ -0,0 +1,22 @@
+//
+// Created by liav on 27/01/17.
+//
+
+#ifndef EX6_JOB_H
+#define EX6_JOB_H
+
+
+class Job {
+private:
+    void *arg;
+    void * (*func)(void *arg);
+public:
+    Job(void * (*func)(void *arg), void* arg);
+    void execute();
+    virtual ~Job();
+};
+
+
+#endif //EX6_JOB_H
+
+
